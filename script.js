4fdd8c583aed9035c99fa7ca3d577f005c0c6d0e
Noel Camacho <ncamacho0@ucol.mx>
2019-01-18 15:56:02 -0600

    Ejemplo de clases y herencia

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+class Forma {
+    constructor (ancho, alto, color){
+        this.ancho = ancho;
+        this.alto = alto;
+        this.color = color;
+    }
+    dibujar(){
+        return document.body.innerHTML = `
+        <div  style = "
+        width: ${this.ancho}px;
+        height: ${this.alto}px;
+        background: ${this.color};"
+        >   
+        </div>
+        
+        `
+  }
+}
+
+class Cuadrado extends Forma {
+    constructor (lado, color){
+        super(lado,lado,color)
+    }
+}
+
+let newCuadrado =  new Cuadrado (200,"red");
